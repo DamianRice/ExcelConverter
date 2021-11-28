8f1e2d9f8c1c6ded29d2081fae67f61421a1d41f
DamianRice <mi007123@163.com>
2021-11-28 13:37:55 +0800

    feat 主框架完成

---
+++ ExcelConverterTests1/ExcelHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExcelConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ExcelConverter.Tests
+{
+    [TestClass()]
+    public class ExcelHandlerTests
+    {
+        [TestMethod()]
+        public void ConvertExcelTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConvertDirectionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CleanGCTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
